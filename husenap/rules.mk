a5772cf28c0a60557764e4f0b439a51741204fd5
Hussein <6496177+Husenap@users.noreply.github.com>
2023-05-12 15:42:46 +0200

    added rules.mk

---
+++ husenap/rules.mk
@@ -0,0 +1,22 @@
+BOOTMAGIC_ENABLE = yes
+MAGIC_ENABLE = yes
+NKRO_ENABLE = yes
+
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+
+SWAP_HANDS_ENABLE = no
+RGBLIGHT_ENABLE = no       
+RGB_MATRIX_ENABLE = no
+BACKLIGHT_ENABLE = no       
+AUDIO_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MOUSEKEY_ENABLE = no
+
+EXTRAKEY_ENABLE = yes         # Audio control and System control
+ 
+SRC += features/achordion.c
+ 
+ 
