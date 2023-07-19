e7ae84a9ff3b7f19093f3425f7418412498b8573
Hussein <6496177+Husenap@users.noreply.github.com>
2023-07-19 13:47:55 +0200

    minor refactor

--- husenap/features/macros.h
+++ husenap/features/macros.h
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-#include "../definitions/keycodes.h"
+#include "definitions/keycodes.h"
 
 bool process_macros(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
